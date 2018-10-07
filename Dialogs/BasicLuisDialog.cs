cd34f07285e7d534c96c2613a9b730b1b08de312
Atul Gupta <P10417496@capita.co.in>
2018-10-07 16:55:33 +0530

    Integrating QnA with for Greeting Intent

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -6,12 +6,24 @@ using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Builder.Luis.Models;
 
+using System.Net.Http;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Text;
+
 namespace Microsoft.Bot.Sample.LuisBot
 {
     // For more information about this template visit http://aka.ms/azurebots-csharp-luis
     [Serializable]
     public class BasicLuisDialog : LuisDialog<object>
     {
+		static string qnamaker_endpointKey = "830e6887-5031-4227-9818-ff4891b44023";
+		static string qnamaker_endpointDomain = "botsharepointsearch";
+		static string HR_kbID = "b664476d-faa0-4709-8dc7-f5e3662bd31c";
+		
+		public QnAMakerService hrQnAService = new QnAMakerService("https://" + qnamaker_endpointDomain + ".azurewebsites.net", HR_kbID, qnamaker_endpointKey);
+
+		
         public BasicLuisDialog() : base(new LuisService(new LuisModelAttribute(
             ConfigurationManager.AppSettings["LuisAppId"], 
             ConfigurationManager.AppSettings["LuisAPIKey"], 
@@ -30,7 +42,10 @@ namespace Microsoft.Bot.Sample.LuisBot
         [LuisIntent("Greeting")]
         public async Task GreetingIntent(IDialogContext context, LuisResult result)
         {
-            await this.ShowLuisResult(context, result);
+			var qnaMakerAnswer = await hrQnAService.GetAnswer(result.Query);
+			await context.PostAsync($"{qnaMakerAnswer}");
+			context.Wait(MessageReceived);
+            //await this.ShowLuisResult(context, result);
         }
 
         [LuisIntent("Cancel")]
@@ -57,4 +72,73 @@ namespace Microsoft.Bot.Sample.LuisBot
             context.Wait(MessageReceived);
         }
     }
+	
+	public class Metadata
+	{
+		public string name { get; set; }
+		public string value { get; set; }
+	}
+
+	public class Answer
+	{
+		public IList<string> questions { get; set; }
+		public string answer { get; set; }
+		public double score { get; set; }
+		public int id { get; set; }
+		public string source { get; set; }
+		public IList<object> keywords { get; set; }
+		public IList<Metadata> metadata { get; set; }
+	}
+
+	public class QnAAnswer
+	{
+		public IList<Answer> answers { get; set; }
+	}
+	
+	[Serializable]
+	public class QnAMakerService
+	{
+		private string qnaServiceHostName;
+		private string knowledgeBaseId;
+		private string endpointKey;
+
+		public QnAMakerService(string hostName, string kbId, string endpointkey)
+		{
+			qnaServiceHostName = hostName;
+			knowledgeBaseId = kbId;
+			endpointKey = endpointkey;
+
+		}
+		async Task<string> Post(string uri, string body)
+		{
+			using (var client = new HttpClient())
+			using (var request = new HttpRequestMessage())
+			{
+				request.Method = HttpMethod.Post;
+				request.RequestUri = new Uri(uri);
+				request.Content = new StringContent(body, Encoding.UTF8, "application/json");
+				request.Headers.Add("Authorization", "EndpointKey " + endpointKey);
+
+				var response = await client.SendAsync(request);
+				return  await response.Content.ReadAsStringAsync();
+			}
+		}
+		public async Task<string> GetAnswer(string question)
+		{
+			string uri = qnaServiceHostName + "/qnamaker/knowledgebases/" + knowledgeBaseId + "/generateAnswer";
+			string questionJSON = @"{'question': '" + question + "'}";
+
+			var response = await Post(uri, questionJSON);
+
+			var answers = JsonConvert.DeserializeObject<QnAAnswer>(response);
+			if (answers.answers.Count > 0)
+			{
+				return answers.answers[0].answer;
+			}
+			else
+			{
+				return "No good match found.";
+			}
+		}
+	}	
 }
