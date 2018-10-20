cd22ea10f4bd167b93aef3d6e70b7198c3362ce2
Atul Gupta <P10417496@capita.co.in>
2018-10-20 14:28:04 +0530

    Changes to fetch the Entity keyword

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -10,6 +10,11 @@ using System.Net.Http;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using System.Text;
+using System.Security;
+using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Client.Search.Query;
+using Microsoft.Bot.Connector;
+
 
 namespace Microsoft.Bot.Sample.LuisBot
 {
@@ -20,8 +25,27 @@ namespace Microsoft.Bot.Sample.LuisBot
 		static string qnamaker_endpointKey = "830e6887-5031-4227-9818-ff4891b44023";
 		static string qnamaker_endpointDomain = "botsharepointsearch";
 		static string HR_kbID = "b664476d-faa0-4709-8dc7-f5e3662bd31c";
-		
-		public QnAMakerService hrQnAService = new QnAMakerService("https://" + qnamaker_endpointDomain + ".azurewebsites.net", HR_kbID, qnamaker_endpointKey);
+
+        private const string EntityKeyword = "SearchKeyword";
+
+
+        private static readonly Dictionary<string, string> PropertyMappings
+        = new Dictionary<string, string>
+        {
+            { "SearchKeyword", "kbkeyword" }
+        };
+
+        [Serializable]
+        public class PartialMessage
+        {
+            public string Text { set; get; }
+        }
+
+
+
+        private PartialMessage message;
+
+        public QnAMakerService hrQnAService = new QnAMakerService("https://" + qnamaker_endpointDomain + ".azurewebsites.net", HR_kbID, qnamaker_endpointKey);
 
 		
         public BasicLuisDialog() : base(new LuisService(new LuisModelAttribute(
@@ -48,22 +72,130 @@ namespace Microsoft.Bot.Sample.LuisBot
             //await this.ShowLuisResult(context, result);
         }
 
-        [LuisIntent("Cancel")]
-        public async Task CancelIntent(IDialogContext context, LuisResult result)
-        {
-            await this.ShowLuisResult(context, result);
-        }
-
-        [LuisIntent("Help")]
-        public async Task HelpIntent(IDialogContext context, LuisResult result)
-        {
-            await this.ShowLuisResult(context, result);
-        }
-		
 		[LuisIntent("FindDocumentation")]
         public async Task FindIntent(IDialogContext context, LuisResult result)
         {
-            await this.ShowLuisResult(context, result);
+            var targetSite = new Uri("https://abcatul.sharepoint.com");
+            var login = "atul.gupta@ABCAtul.onmicrosoft.com";
+            var password = "Nilima1@05";
+            StringBuilder sbQuery = new StringBuilder();
+            bool QueryTransformed = false;
+            var searchKeyword = "";
+            if (result.Entities.Count > 0)
+            {
+                QueryTransformed = true;
+                EntityRecommendation keywordEntityRecommendation;
+
+                if (result.TryFindEntity(EntityKeyword, out keywordEntityRecommendation))
+                {
+                    searchKeyword = keywordEntityRecommendation.Entity;
+                    sbQuery.Append(searchKeyword);
+                }
+               
+            }
+            else
+            {
+                //should replace all special chars
+                sbQuery.Append(this.message.Text.Replace("?", ""));
+            }
+
+            var reply = context.MakeMessage();
+            reply.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+            reply.Attachments = new List<Microsoft.Bot.Connector.Attachment>();
+
+            var securePassword = new SecureString();
+            foreach (char c in password)
+            {
+                securePassword.AppendChar(c);
+            }
+            var onlineCredentials = new SharePointOnlineCredentials(login, securePassword);
+
+            using (ClientContext clientContext = new ClientContext(targetSite))
+            {
+                clientContext.Credentials = onlineCredentials;
+
+                KeywordQuery query = new KeywordQuery(clientContext);
+                query.QueryText = string.Concat(sbQuery.ToString(), " IsDocument:1");
+                //query.QueryText = "test";
+                query.RowLimit = 5;
+
+                SearchExecutor searchExecutor = new SearchExecutor(clientContext);
+                ClientResult<ResultTableCollection> resultShare = searchExecutor.ExecuteQuery(query);
+                clientContext.ExecuteQuery();
+                if (resultShare.Value != null && resultShare.Value.Count > 0 && resultShare.Value[0].RowCount > 0)
+                {
+                    reply.Text += (QueryTransformed == true) ? "I found some interesting reading for you!" : "I found some potential interesting reading for you!";
+                    BuildReply(resultShare, reply);
+                }
+                else
+                {
+                    if (QueryTransformed)
+                    {
+                        //fallback with the original message
+                        query.QueryText = string.Concat(this.message.Text.Replace("?", ""), " IsDocument:1");
+                        query.RowLimit = 3;
+                        searchExecutor = new SearchExecutor(clientContext);
+                        resultShare = searchExecutor.ExecuteQuery(query);
+                        clientContext.ExecuteQuery();
+                        if (resultShare.Value != null && resultShare.Value.Count > 0 && resultShare.Value[0].RowCount > 0)
+                        {
+                            reply.Text += "I found some potential interesting reading for you!";
+                            BuildReply(resultShare, reply);
+                        }
+                        else
+                            reply.Text += "I could not find any interesting document!";
+                    }
+                    else
+                        reply.Text += "I could not find any interesting document!";
+
+                }
+                await context.PostAsync(reply);
+                context.Wait(MessageReceived);
+            }
+        }
+
+        void BuildReply(ClientResult<ResultTableCollection> results, IMessageActivity reply)
+        {
+            foreach (var row in results.Value[0].ResultRows)
+            {
+                List<CardAction> cardButtons = new List<CardAction>();
+                List<CardImage> cardImages = new List<CardImage>();
+                string ct = string.Empty;
+                string icon = string.Empty;
+                switch (row["FileExtension"].ToString())
+                {
+                    case "docx":
+                        ct = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+                        icon = "https://cdn2.iconfinder.com/data/icons/metro-ui-icon-set/128/Word_15.png";
+                        break;
+                    case "xlsx":
+                        ct = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                        icon = "https://cdn2.iconfinder.com/data/icons/metro-ui-icon-set/128/Excel_15.png";
+                        break;
+                    case "pptx":
+                        ct = "application/vnd.openxmlformats-officedocument.presentationml.presentation";
+                        icon = "https://cdn2.iconfinder.com/data/icons/metro-ui-icon-set/128/PowerPoint_15.png";
+                        break;
+                    case "pdf":
+                        ct = "application/pdf";
+                        icon = "https://cdn4.iconfinder.com/data/icons/CS5/256/ACP_PDF%202_file_document.png";
+                        break;
+
+                }
+                cardButtons.Add(new CardAction
+                {
+                    Title = "Open",
+                    Value = (row["ServerRedirectedURL"] != null) ? row["ServerRedirectedURL"].ToString() : row["Path"].ToString(),
+                    Type = ActionTypes.OpenUrl
+                });
+                cardImages.Add(new CardImage(url: icon));
+                ThumbnailCard tc = new ThumbnailCard();
+                tc.Title = (row["Title"] != null) ? row["Title"].ToString() : "Untitled";
+                tc.Text = (row["Description"] != null) ? row["Description"].ToString() : string.Empty;
+                tc.Images = cardImages;
+                tc.Buttons = cardButtons;
+                reply.Attachments.Add(tc.ToAttachment());
+            }
         }
 
         private async Task ShowLuisResult(IDialogContext context, LuisResult result) 
@@ -140,5 +272,7 @@ namespace Microsoft.Bot.Sample.LuisBot
 				return "No good match found.";
 			}
 		}
-	}	
+
+        
+    }	
 }
